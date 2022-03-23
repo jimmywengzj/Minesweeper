6f0532816df0951ca2d4ec29ec1a2c8df3e5d025
ssc <101316872+ssc08@users.noreply.github.com>
2022-03-23 22:23:35 +0100

    加入读取properties文件的程序

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,9 +1,13 @@
 package src;
+import  java.util.Locale;
+import  java.util.ResourceBundle;
 
 public class Minesweeper {
     
     public static void main(String[] args) {
         new menu2();
         
+        
     }
+    
 }
